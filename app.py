8e8d147ac5a41182686321b652bc179f6c61e329
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-05-30 23:56:39 +0500

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     st.title("Web Scraper Demo")
 
     # Make a GET request to the website
-    url = "http://quotes.toscrape.com/"  # Replace with the URL of the website you want to scrape
+    url = "https://www.imdb.com/chart/top"  # Replace with the URL of the website you want to scrape
     response = requests.get(url)
 
     # Extract the links using regular expressions
