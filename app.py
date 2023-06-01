41a769c91abe243ab62b7bc0e1f6300f6b4f9f9f
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-06-01 09:46:10 +0500

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import streamlit as st
 import requests
-import re
+from bs4 import BeautifulSoup
 
 def main():
     st.title("Movie Title Scraper")
 
     # Get the URL from the user
-    url = st.text_input("Enter the URL", "https://example.com/movies")
+    url = st.text_input("Enter the URL", "https://www.imdb.com/chart/top")
 
     # Make a GET request to the website
     response = requests.get(url)
 
-    # Extract the movie titles using regular expressions
-    movie_titles = re.findall(r'<h2>(.*?)</h2>', response.text)
+    # Create BeautifulSoup object
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extract the movie titles
+    movie_titles = soup.find_all('td', class_='titleColumn')
 
     # Display the extracted movie titles in Streamlit
     for title in movie_titles:
-        st.write(title)
+        movie_name = title.a.text
+        st.write(movie_name)
 
 if __name__ == '__main__':
     main()
