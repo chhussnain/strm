c8ba6e32a0f0ef4df2d53b390b742c32c19f8458
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-06-01 12:44:04 +0500

    APP.py

--- APP.py
+++ APP.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-import re
+from bs4 import BeautifulSoup
 
 def main():
     st.title("My Web Scraper")
@@ -13,12 +13,19 @@ def main():
         # Make a GET request to the website
         response = requests.get(url)
 
-        # Extract the class attributes using regular expressions
-        classes = re.findall(r'class=["\']([^"\']*)', response.text)
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        # Display the extracted classes in Streamlit
-        for class_name in classes:
-            st.write(class_name)
+        # Find all the input fields in the HTML
+        input_fields = soup.find_all("input")
+
+        # Display the extracted input fields in Streamlit
+        for input_field in input_fields:
+            input_name = input_field.get("name")
+            input_type = input_field.get("type")
+            st.write("Name:", input_name)
+            st.write("Type:", input_type)
+            st.write("-----")
 
 if __name__ == '__main__':
     main()
