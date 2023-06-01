bc454b822af684391a42e510811d5a162a6b9166
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-06-01 12:38:44 +0500

    APP.py

--- APP.py
+++ APP.py
@@ -3,20 +3,22 @@ import requests
 import re
 
 def main():
-    st.title(" My Web Scraper")
+    st.title("My Web Scraper")
 
     # Get the URL from the user
-    url = st.text_input("Enter the URL", "https://quotes.toscrape.com/")
+    url = st.text_input("Enter the URL", "https://example.com")
 
-    # Make a GET request to the website
-    response = requests.get(url)
+    # Add a button to trigger scraping
+    if st.button("Scrape Website"):
+        # Make a GET request to the website
+        response = requests.get(url)
 
-    # Extract the links using regular expressions
-    links = re.findall(r'<a\s+href=[\'"]?([^\'" >]+)', response.text)
+        # Extract the button elements using regular expressions
+        buttons = re.findall(r'<button[^>]*>(.*?)</button>', response.text, re.IGNORECASE)
 
-    # Display the extracted links in Streamlit
-    for link in links:
-        st.write(link)
+        # Display the extracted buttons in Streamlit
+        for button in buttons:
+            st.write(button)
 
 if __name__ == '__main__':
     main()
